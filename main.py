a0d3d1a522b248d7fd636a53ec3441be09d76300
Yago Maia <yagohenriquev123@gmail.com>
2024-03-12 16:28:25 -0300

    Comentando funções do fastapi e celery, colocando indagações nos arquivos para futura implementação no tarrafa

--- main.py
+++ main.py
@@ -1,48 +1,71 @@
 import tasks
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI, BackgroundTasks, Form
 from celery.result import AsyncResult
 from celery import group
-
-#* Pode fazer uma função puxando o app do celery para o endpoint, creio que não seria bom...
-# def get_task_by_id(task_id):
-#     ret = tasks.celery_app.AsyncResult(task_id)
-#     return ret
 
 #* Pra juntar isso com o FastAPI tem que colocar essa execução de tasks no background
 
 app = FastAPI()
 
 @app.get("/")
-def home():
+def home() -> str:
+    """
+    Endpoint responsável por gerar a Home Page
+    """
     return "Home Page"
 
 @app.post("/soma")
-def task_soma(num1 : int, num2 : int):
+def task_soma(num1 : int = Form(), num2 : int = Form()) -> AsyncResult.id:
+    """
+    Endpoint responsável por somar dois números, feito pelo celery
+    """
     soma = tasks.add.delay(num1, num2)
     return soma.id
 
 @app.post("/hello_world")
-def hello_world():
+def hello_world() -> AsyncResult.id:
+    """
+    Endpoint responsável por retornar Hello World, feito pelo celery
+    """
     hello = tasks.hello.delay()
     return hello.id
 
 @app.post("/inserir_redis")
-def inserir_dados_redis(chave, valor):
+def inserir_dados_redis(chave: str = Form(), valor: str | int = Form()) -> AsyncResult.id:
+    """
+    Endpoint responsável por inserir um par chave-valor no redis, feito pelo celery
+    """
     setar_redis = tasks.inserir.delay(chave, valor)
     return setar_redis.id
 
-#* Tentar deixar nessa estrutura await kwargs['redis'].setField(universidade, path, ret)
-#* Criar uma tabela contendo os endpoints? -> E se mudar? muito trabalho
-#* Eu precisaria do path independente
-#* Precisaria do id_ies, do path, e do retorno
-#* Uso requests para acessar os endpoints?
-#* Daí faço uma dict contendo o path e o retorno?
-#* Precisaria de uma lista com todos os endpoint no minimo
-#* Valeria a pena reinserir no redis as principais rotas, tipo aquelas que mais usam que não usam parametros na url
+@app.post("/apagar_campo_redis")
+def apagar_campo_redis(chave : str = Form()) -> AsyncResult.id:
+    """
+    Endpoint responsável por deletar um campo associado a uma chave no redis, feito pelo celery
+    """
+    deletar_campo = tasks.deletar.delay(chave)
+    return deletar_campo.id
+
+#TODO Apagar Endpoints
 #* Ter a opção de limpar todas as rotas ou de limpar uma rota específica seria bom
+#* A função de apagar TODOS os endpoints existentes lá seria fácil, só dar um flushdb
+#* Para apagar de uma universidade também seria fácil, seria só apagar a chave que tem o id_ies dela
+#* Para apagar um endpoint especifício poderia pegar todas os campos que estão vinculados a universidade e apagar o campo
+#* Teria endpoints repetidos, mudando apenas o parâmetro na URL
+#* Olhar se o redis tem alguma função tipo o like do Postgres aí daria pra apagar todos os endpoints, mesmo com as variações de parâmetros
+
+#TODO Inserir Endpoints
+#? A parte de inserir é a que complica
+#? A parte de inserir não seria literalmente acessar o endpoint via docs do fastapi ou chamar um requisão normal ??
+#? A questão de recolocar os endpoints no redis seria a questão dos parâmetros...
+#? Algumas opções seriam acessar o sistema normal, mas pra uma grande quantidade de universidades não faria sentido
+#? Outra opção mais viável seria criar um módulo que faria as principais requisições...
 
 @app.post("/background/inserir_redis")
-def inserir_dados_redis(chave, valor, background_task : BackgroundTasks):
+def inserir_dados_redis(*, chave: str = Form(), valor: str | int = Form(), background_task : BackgroundTasks) -> str:
+    """
+    Endpoint responsável por inserir um par chave-valor em que é feito em Background Task pelo celery
+    """
     try:
         background_task.add_task(tasks.inserir.delay, chave, valor)
         return "Função inserir dados no redis sendo executada em Background"
@@ -50,12 +73,15 @@ def inserir_dados_redis(chave, valor, background_task : BackgroundTasks):
         return "Erro ao executar função inserir dados em Background"
 
 @app.post("/group")
-def task_group_soma():
+def task_group_soma() -> list:
+    """
+    Endpoint responsável por realizar a soma de dois números com o método group do celery.
+    """
     tasks_list = []
     data = []
     
     for b in range(0,5):
-        tasks_list.append(tasks.add.s(b, b+2)) #.s Seria o que exatamente?
+        tasks_list.append(tasks.add.s(b, b+2)) #! .s Seria o que exatamente?
     
     job = group(tasks_list)
     
@@ -65,47 +91,14 @@ def task_group_soma():
         data.append(r)
     return data
 
-# @app.post("/group/requests")
-# def task_group_requests(background_task : BackgroundTasks):
-    
-#     try:
-#         tasks_list = []
-#         #data = []
-#         lista_rotas = retorna_rotas()
-        
-#         #for r in lista_rotas:
-#         #    tasks_list.append(tasks.request_sistema.s(r['path'], r['method'])) #.s Seria o que exatamente?
-        
-#         #job = group(tasks_list)
-        
-#         #result = job.apply_async()
-#         for r in lista_rotas:
-#             background_task.add_task(tasks.request_sistema.delay, r['path'], r['method'])
-        
-#         return "Executando a função em background"
-#         #ret_values = result.get(disable_sync_subtasks=False)
-#         #for r in ret_values:
-#         #    data.append(r)
-#         #return data
-#     except Exception as error:
-#         return "Erro ao executar função em background"
-
 @app.get("/task/{task_id}")
-def get_task(task_id):
+def get_task(task_id : str) -> dict:
+    """
+    Endpoint responsável por retoranr os dados de uma task do celery.
+    """
     ret = tasks.get_task_by_id(task_id)
     return {
         'Id' : task_id,
         'Status': ret.status,
         'Result': ret.get()
     }
-
-#* Não é todas as rotas que devem ser inseridas no redis
-#! Isso daqui não vai dar certo
-# @app.get("/rotas")
-# def retorna_rotas():
-#     rotas_lista = []
-#     for r in app.routes:
-#         if "HEAD" not in r.methods and "GET" in r.methods and "/rotas" not in r.path and "{" not in r.path:
-#             for m in r.methods:
-#                 rotas_lista.append({'path':r.path, 'name': r.name, 'method' : m})
-#     return rotas_lista
--- tasks.py
+++ tasks.py
@@ -1,59 +1,94 @@
 from celery import Celery, Task
+from celery.result import AsyncResult
 import redis
 import os
-from requests import Session, request
 
-URL = "http://localhost:8000"
-
-r = redis.Redis()
+redis_connection = redis.Redis()
 
 os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
+BROKER_URL = 'redis://localhost:6379/1'
+BACKEND_URL = 'redis://localhost:6379/1'
+
+#* celery -A tasks worker -l info -P gevent
+#* celery -A tasks worker --pool=solo -l info
 
 class DebugTask(Task):
-
+    """
+    Classes responsável por informar sobre a Task
+    """
     def __call__(self, *args, **kwargs):
         print('TASK STARTING: {0.name}[{0.request.id}]'.format(self))
         return self.run(*args, **kwargs)
 
-#* celery -A tasks worker -l info -P gevent
-
-BROKER_URL = 'redis://localhost:6379/1'
-BACKEND_URL = 'redis://localhost:6379/1'
+#* Inicializando o Celery
 celery_app = Celery('tasks', broker=BROKER_URL, backend=BACKEND_URL, broker_connection_retry_on_startup = True)
 
+# @celery_app.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     # Calls test('hello') every 10 seconds.
+#     senderedis_connection.add_periodic_task(10.0, hello.s(), name='add every 10')
+
 @celery_app.task(name = "Soma de dois números", base = DebugTask)
-def add(x, y):
+def add(x: int, y: int) -> int:
+    """
+    Função responsável por somar dois números
+    
+    x: Primeiro número da soma
+    y: Segundo número da soma
+    """
     return x + y
 
-@celery_app.task(name = "Hello World", base = DebugTask)
-def hello():
+@celery_app.task(name = "Hello World", base = DebugTask, ignore_result = True)
+def hello() -> str:
+    """
+    Função responsável por retornar hello world
+    """
     return "Hello World"
 
 @celery_app.task(name = "Inserir Valor redis", base = DebugTask)
-def inserir(chave, valor):
-    ret = r.set(chave, valor)
+def inserir(chave, valor) -> bool:
+    """
+    Função responsável por setar um par chave-valor no redis
+    
+    chave: Chave que será setada no redis
+    valor: Valor que será associado a chave
+    """
+    ret = redis_connection.hset("celery_fastapi", chave, valor)
+    if ret:
+        return True
+    return False
+
+@celery_app.task(name = "Deletar campo redis", base = DebugTask)
+def deletar(chave) -> bool:
+    """
+    Função responsável por deletar um campo  associado a uma chave no redis
+    
+    chave: Chave que será deletada no redis
+    """
+    ret = redis_connection.hdel("celery_fastapi", chave)
     if ret:
         return True
     return False
 
 @celery_app.task(name = "Limpar Cache redis", base = DebugTask)
-def flush_redis():
-    ret = r.flushdb()
+def flush_redis() -> str:
+    """
+    Função responsável por dar um flushdb no redis
+    """
+    ret = redis_connection.flushdb()
     if ret:
         return "Chaves dos Redis limpas"
     return "Erro ao limpar chaves"
 
-#* Celery deve só funcionar para funções básicas e funções que envolvam o redis
-@celery_app.task(name = "Request Sistema", base = DebugTask)
-def request_sistema(path:str, method:str):
-    if method == "GET":
-        ret = request(method, f"{URL}{path}")
-        return ret.status_code
-        if ret:
-            return True
-        return False
-
-#* Pode fazer assim deixando os métodos relacioandos a celery tudo em um arquivo só...
-def get_task_by_id(task_id):
+def get_task_by_id(task_id) -> AsyncResult:
+    """
+    Função responsável por pegar os dados de um tarefa no celery, dado seu id
+    
+    task_id : Id da Task do celery
+    """
     ret = celery_app.AsyncResult(task_id)
     return ret
+
+#TODO Estudar -> Linking (callbacks/errbacks) 
+#TODO Estudar -> ETA and Countdown
+#TODO Estudar -> Routing options add.apply_async(queue='priority.high')
